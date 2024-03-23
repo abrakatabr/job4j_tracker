a195dae4b4c1e5b9c8ffaaa93eb8d77095419740
Pozhar <abrakatabr@mail.ru>
2024-03-23 17:18:52 +0300

    3. Отсортировать департаменты [#34352 #485404]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -7,16 +7,7 @@ public class DepartmentsDescComparator implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] o1Split = o1.split("/");
         String[] o2Split = o2.split("/");
-        int minLength = Math.min(o1Split.length, o2Split.length);
-        if (!o1Split[0].equals(o2Split[0])) {
-            return o2Split[0].compareTo(o1Split[0]);
-        }
-        for (int i = 1; i < minLength; i++) {
-            if (o2Split[i].equals(o1Split[i])) {
-                continue;
-            }
-            return o1Split[i].compareTo(o2Split[i]);
-        }
-        return Integer.compare(o1Split.length, o2Split.length);
+        int result = o2Split[0].compareTo(o1Split[0]);
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
